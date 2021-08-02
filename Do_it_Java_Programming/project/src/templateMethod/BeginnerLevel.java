4eb72adade12799bd4b3f68d663529d474a352b1
kimjh <tjsej1324@gmail.com>
2021-08-02 23:34:22 +0900

    Do It 자바 프로그래밍 9장 추상클래스

---
+++ Do_it_Java_Programming/project/src/templateMethod/BeginnerLevel.java
@@ -0,0 +1,28 @@
+package templateMethod;
+
+public class BeginnerLevel extends PlayerLevel {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("천천히 달립니다.");
+	}
+
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+		System.out.println("Jump할 줄 모르지롱.");
+	}
+
+	@Override
+	public void turn() {
+		// TODO Auto-generated method stub
+		System.out.println("Turn할 줄 모르지롱.");
+	}
+	
+	@Override
+	public void showLevelMessage() {
+		// TODO Auto-generated method stub
+		System.out.println("**** 초급자 레벨입니다. *****");
+	}	
+}
