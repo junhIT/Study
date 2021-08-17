f3d1c062d007df66004cb136e546468ac67fc7a2
kimjh <tjsej1324@gmail.com>
2021-08-18 00:54:58 +0900

    12장 컬렉션 프레임워크

--- Do_it_Java_Programming/project/src/class12/GenericTest.java
+++ Do_it_Java_Programming/project/src/class12/GenericTest.java
@@ -4,11 +4,11 @@ import class7.array.Student;
 
 public class GenericTest {
 	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GenericTest gt = new GenericTest();
-		gt.genericPrinterClass();
+//		gt.genericPrinterClass();
+		gt.genericMethod();
 	}
 	
 	public void generic() {
@@ -25,12 +25,32 @@ public class GenericTest {
 		
 		powderPrinter.setMaterial(new Powder());
 		Powder powder = powderPrinter.getMaterial();
-		System.out.println(powderPrinter);
+		powder.doPrinting();
 		
 		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
 		
 		plasticPrinter.setMaterial(new Plastic());
 		Plastic plastic = plasticPrinter.getMaterial();
-		System.out.println(plasticPrinter);
+		plastic.doPrinting();
+	}
+	
+	public static <T, V> double makeRectangle(Point<T, V> p1, Point<T, V> p2) {
+		double left = ((Number)p1.getX()).doubleValue();
+		double right = ((Number)p2.getX()).doubleValue();
+		double top = ((Number)p1.getY()).doubleValue();
+		double bottom = ((Number)p2.getY()).doubleValue();
+		
+		double width = right - left;
+		double height = bottom - top;
+		
+		return width * height;
+	}
+	
+	public void genericMethod() {
+		Point<Integer, Double> p1 = new Point<Integer, Double>(0, 0.0);
+		Point<Integer, Double> p2 = new Point<>(10, 10.0);
+		
+		double rect = makeRectangle(p1, p2);
+		System.out.println("두 점으로 만들어진 사각형의 넓이는 " + rect + "입니다.");
 	}
 }
--- Do_it_Java_Programming/project/src/class12/Material.java
+++ Do_it_Java_Programming/project/src/class12/Material.java
@@ -2,7 +2,4 @@ package class12;
 
 public abstract class Material {
 	public abstract void doPrinting();
-	
-	public abstract String toString();
-
 }
