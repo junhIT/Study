e60ef9f443ab4e515525250a62624be68a0fa59e
junhIT <tjsej1324@gmail.com>
2022-06-29 22:35:25 +0900

    CacheManager추가, Cacheable 실습

---
+++ springboot_backend_programming/src/main/java/com/example/demo/config/CacheConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
--- springboot_backend_programming/src/main/java/com/example/demo/controller/SearchController.java
+++ springboot_backend_programming/src/main/java/com/example/demo/controller/SearchController.java
@@ -3,8 +3,10 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,9 @@ import com.example.demo.exception.ClientNoContentRuntimeException;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.service.MovieService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/search")
 public class SearchController {
@@ -26,7 +31,10 @@ public class SearchController {
 	private BookRepository bookRepository;
 
 	@GetMapping("/movies")
+	@Cacheable(key = "#query", cacheNames="List<MovieDTO>")
 	public List<MovieDTO> getMoviesByQuery(@RequestParam(name = "q") String query) {
+
+		log.info("#################################### getMoviesByQuery Method 호출, " + query);
 		
 		List<MovieDTO> movieList = movieService.search(query);
 		
@@ -44,5 +52,19 @@ public class SearchController {
 	@GetMapping("/recommende-movie")
 	public MovieDTO getRecommendeMovie(@RequestParam(name = "q") String query) throws ClientNoContentRuntimeException {
 		return movieService.recommendeTodayMovie(query);
+	}
+	
+	/*
+	 * movieDTO 입력받아 저장하기 위한 Method
+	 */
+	@PostMapping("/insertMovie")
+	public boolean insertMovie(MovieDTO movieDTO) {
+		boolean success = false;
+		
+		log.info("############################# insertMovie Method 호출" + movieDTO.toString());
+		
+		
+		
+		return success;
 	}
 }
