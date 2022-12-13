4978f514acf8d4f34ab5fe73aa19752d87055a1a
junhIT <tjsej1324@gmail.com>
2022-12-14 01:29:02 +0900

    AOP - LogAspect 추가

---
+++ spring-base/src/main/java/com/example/springbase/aop/PerfAspect.java
@@ -0,0 +1,33 @@
+package com.example.springbase.aop;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Aspect
+public class PerfAspect {
+
+	@Around("execution(* com.example.springbase.controller..*.*(..))")
+	public Object logAspect(ProceedingJoinPoint pjp) throws Throwable {
+		
+		Object returnVal = pjp.proceed();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");	
+		
+		String nowDtm = sdf.format(new Date());
+		
+		log.debug("===================================================================");
+		log.debug(nowDtm + "currentProceed = ", pjp.getSignature());
+		log.debug("===================================================================");
+		
+		return returnVal;
+	}
+}
